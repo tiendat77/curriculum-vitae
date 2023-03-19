81bee3964e1bae256d8675a79e487f650ef2eb5a
Ji Woo <huynhztienzdat@gmail.com>
2023-03-20 00:34:12 +0700

    feat: ✨🎨 structure and theme

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,10 @@
  * You can import this data from anywhere in your site by using the `import` keyword.
  */
 
-export default {};
+export const SITE = {
+  website: "https://tiendathuynh.netlify.app",
+  author: "Tien Dat Huynh <huynhztienzdat@gmail.com>",
+  description: "Tien Dat Huynh's Resume",
+  title: "Tien Dat Huynh's Resume",
+};
+
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,52 @@
 /** @type {import('tailwindcss').Config} */
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true
+      },
+      textColor: {
+        skin: {
+          base: withOpacity("--text-color-base"),
+          accent: withOpacity("--text-color-accent"),
+        }
+      },
+      backgroundColor: {
+        skin: {
+          base: withOpacity("--background-color-base"),
+          card: withOpacity("--background-color-card"),
+        }
+      },
+      fontFamily: {
+        sans: ['Fira Sans Condensed', 'sans-serif']
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100ch',
+            a: {
+              color: 'rgb(var(--text-color-accent))',
+              textDecoration: 'none',
+              '&:hover': {
+                textDecoration: 'underline'
+              }
+            }
+          }
+        }
+      }
+    }
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography")]
 };
